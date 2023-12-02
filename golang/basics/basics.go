9e667cb4fed5a2e8f81b0f27deeb601be6270ad1
iamshrivishnu <srivishnu31@gmail.com>
2023-12-02 21:49:48 +0530

    2023 - Day 1

--- golang/basics/basics.go
+++ golang/basics/basics.go
@@ -24,3 +24,25 @@ func GetLargestOfThree(number1 int, number2 int, number3 int) int {
 	intermediateMax := math.Max(float64(number1), float64(number2))
 	return int(math.Max(intermediateMax, float64(number3)))
 }
+
+func ReverseString(str string) (result string) {
+	for _, v := range str {
+		result = string(v) + result
+	}
+	return
+}
+
+func ReverseArrayOfStrings(strings []string) []string {
+	for i := 0; i < len(strings)/2; i++ {
+		j := len(strings) - i - 1
+		strings[i], strings[j] = strings[j], strings[i]
+	}
+	return strings
+}
+
+func SumOfValues(array []int) (sum int) {
+	for _, value := range array {
+		sum += value
+	}
+	return
+}
